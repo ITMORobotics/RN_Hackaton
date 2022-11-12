6715c63508add8095b5da2c09dc6a7c4d5ebccc2
TexnoMan <antipov.vl.a@yandex.ru>
2022-11-12 12:55:40 +0500

    Add stend class

---
+++ tests/test_analyzer.py
@@ -0,0 +1,21 @@
+import re
+import time
+from rn_hackaton.stend.analyzer import SerialReader
+
+def main():
+    ser = SerialReader()
+    try:
+        ser.start_thread()
+        time.sleep(1)
+        while True:
+            print('-'*50)
+            print('time:  ', ser.scale_time_update)
+            print("scale: ", ser.scale)
+            print("ir   : ", ser.IRS)
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        ser.stop_thread()
+
+
+if __name__=="__main__":
+    main()
