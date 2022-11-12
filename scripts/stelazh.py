06e52cb44ebe5e149533582814b3b49796e97eda
Dmitrii <CherginetsDA@yandex.ru>
2022-11-12 19:46:00 +0300

    Fix colibration

--- scripts/stelazh.py
+++ scripts/stelazh.py
@@ -7,12 +7,12 @@ class stelazh:
         self.addr = np.array([  [[0,0,0],[1,0,0],[2,0,0]],
                                 [[0,0,-1],[1,0,-1],[2,0,-1]],
                                 [[0,0,-2],[1,0,-2],[2,0,-2]]])
-        self.KK = np.diag([86,0,110])
+        self.KK = np.diag([110,0,86])
         self.left_up_point = np.array([0]*3)
         self.kx = 1
         self.ky = 1
         self.kz = 1
-        self.phi = -0.1
+        self.phi = 0
 
 
     def __getitem__(self,idx):
@@ -28,13 +28,17 @@ class stelazh:
             return self.left_up_point + R@self.KK@self.addr[idx]
 
     def calibrate(self,point_left_up, point_rigth_up):
-        temp = point_left_up - point_rigth_up
-        self.phi = np.atan2(temp[0],temp[1])
+        temp = point_rigth_up - point_left_up
+        self.phi = np.arctan2(temp[1],temp[0])
+        self.left_up_point = point_left_up
         
 
 def main():
     st = stelazh()
-    print(st[2][1])
+    st.calibrate(np.array([666.53,204.85,543.56]),np.array([893.01,190.39,543.97]))
+    print(st[0][0])
+    print(st[0][1])
+    print(st[1][0])
 
 if __name__=="__main__":
     main()
